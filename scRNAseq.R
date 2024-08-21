9037a18e6c95231d5697b24139126cf086ebca79
ec-evanmv <evanmv@uio.no>
2024-08-21 14:30:35 +0200

    Finally got data into sparse.matrix and created Seurat object, QC, filtered, normalized

--- scRNAseq.R
+++ scRNAseq.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 remotes::install_version('Matrix', version = '1.6.4') #Required for installing Seurat
 install.packages("Seurat")
+library(Seurat)
 library(SeuratObject)
-library(dplyr)
 library(patchwork)
 library(Matrix)
 
@@ -10,20 +10,43 @@ library(Matrix)
 UMIcounts <- read_delim('GSE181919_UMI_counts.txt') #Didn't work, too large I think. 
 UMI_counts <- data.table::fread('GSE181919_UMI_counts.txt')
 
-head(UMI_counts[, 1:10])
-rownames(UMI_counts) <- UMI_counts$V1 #Set gene names from column V1 as row names
-
-#Convert data.frame to matrix
-UMI.m <- as.matrix(UMI_counts)
-rownames(UMI.m) <- UMI_counts$V1 #Assign gene names as row names
-UMI.m <- UMI.m[, colnames(UMI.m) != "V1"] #Remove row names as sep column
-head(UMI.m[, 1:10])
-rownames(UMI.m)
-colnames(UMI.m)
+head(UMI.mat[,1:10])
+rnames <- as.vector(UMI_counts$V1) #Saves gene names as character vector
+UMI_count.df <- as.data.frame(UMI_counts)
+UMI.mat <- data.matrix(UMI_count.df[,2:ncol(UMI_count.df)]) #Creates matrix and drops gene names as column
+rownames(UMI.mat) <- rnames #Assigns character vector of gene names as row names
+UMI.mat <- as.sparse(UMI.mat) #Necessary for
 #Remember to free unused memory
 #Create Seurat object
 ?Seurat
-Sobject <- CreateSeuratObject(counts = UMI.m, project = "Bish", min.features = 200, min.cells = 3)
+Sobject <- CreateSeuratObject(counts = UMI.mat, project = "Bish", min.features = 200, min.cells = 3)
 
+#A few stats on matrix
 dim(UMI.m)
 dimnames(UMI.m)
+class(UMI.mat)
+sum(duplicated(rownames(UMI.mat)))
+
+#Stats on seurat object
+dim(Sobject)
+class(Sobject)               
+head(Sobject[,1:10])
+
+##QC and filtering -----
+
+Sobject[["percent.mt"]] <- PercentageFeatureSet(Sobject, pattern = "^MT-") #Adds column with % of mitochondrial genes
+head(Sobject, 5)
+
+#Visualize QC parameters
+VlnPlot(Sobject, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+plot1 <- FeatureScatter(Sobject, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+
+#Filtering
+
+Sobject <- subset(Sobject, subset = nFeature_RNA > 200 & nFeature_RNA < 8000 & percent.mt < 10)
+
+#Normalization
+
+Sobject <- NormalizeData(Sobject, normalization.method = "LogNormalize", scale.factor = 10000)
+
